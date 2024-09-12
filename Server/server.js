034803714fe35390ba7079760798390adc37c05b
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2024-09-12 23:41:55 +0530

    yeahsd

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Enable CORS for all routes
+app.use(cors('*')); // Enable CORS for all routes
 app.use(bodyParser.json());
 app.use(
   helmet({
